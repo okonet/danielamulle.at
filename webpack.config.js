7d76c46e7066f0993a7ff47d8af7fa699e4d5d4d
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2014-12-02 23:09:17 +0100

    Updated sass-loader config

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
           "style-loader",
           "css-loader",
           "autoprefixer-loader?browsers=last 2 version",
-          "sass-loader"
+          "sass-loader?imagePath=./css"
         ]
       },
       { test: /\.(png|svg|jpg)$/, loader: "url?limit=15000" }
