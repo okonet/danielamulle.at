2debd697b7fab5421c5f12a98385fbef2019ba8f
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-01-06 19:46:24 +0100

    Added importPaths to sass-loader config

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
           "style-loader",
           "css-loader",
           "autoprefixer-loader?browsers=last 2 version",
-          "sass-loader?imagePath=./css"
+          "sass-loader?imagePath=./css&includePaths[]=./src/css"
         ]
       },
       { test: /\.(png|svg|jpg)$/, loader: "url?limit=15000" },
