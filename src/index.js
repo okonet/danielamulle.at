3c585db793043143ad968a50a59510641521182e
Andrey Okonetchnikov <andrej.okonetschnikow@gmail.com>
2015-01-11 13:15:24 +0100

    Experimenting with content and layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 require('./styles.scss');
+
+var d = document
+var links = d.querySelectorAll('h2 a')
+Array.prototype.slice.call(links).forEach(function(link) {
+  link.addEventListener('click', function(event) {
+    // event.preventDefault()
+    activeSection = d.querySelector('section.active')
+
+    var href = event.target.attributes['href'].value
+    var id = href.replace('#', '')
+    var section = d.getElementById(id)
+    section.classList.toggle('active')
+
+    if (activeSection !== null && activeSection.id !== id) {
+      activeSection.classList.remove('active')
+    }
+
+  })
+})
