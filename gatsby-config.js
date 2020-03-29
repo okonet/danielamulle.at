1fb5f06c76a7367207a1207eb90c3ccdb9dc7f9e
Andrey Okonetchnikov <andrey@okonet.ru>
2020-03-29 15:17:29 +0200

    Setup GatsbyJS

---
+++ gatsby-config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  siteMetadata: {
+    title: `My Blog Title`,
+
+    author: `My Name`,
+
+    description: `My site description...`,
+
+    social: [
+      {
+        name: `Twitter`,
+        url: `https://twitter.com/gatsbyjs`,
+      },
+      {
+        name: `GitHub`,
+        url: `https://github.com/gatsbyjs`,
+      },
+    ],
+  },
+  plugins: [
+      'gatsby-plugin-netlify-cms',
+    {
+      resolve: "gatsby-theme-ui-blog",
+      options: {
+        basePath: `/blog`
+      }
+    },
+    "gatsby-plugin-theme-ui",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/images`
+      }
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/gatsby-icon.png" // This path is relative to the root of the site.
+      }
+    }
+  ]
+};
--- index.js
+++
@@ -1,9 +0,0 @@
-// Client-side render
-if (typeof document !== 'undefined') {
-    require('./src/index.js');
-}
-// Exported static site renderer:
-module.exports = function render(locals, callback) {
-    //console.log();
-    callback(null, locals.html);
-};
