cbcd087d31257ad5ad921dc1ab90f0c68ae0ba5d
Andrey Okonetchnikov <andrey@okonet.ru>
2020-04-01 09:43:11 +0200

    Fix images

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,15 +54,15 @@ module.exports = {
         name: "content/assets",
       },
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/images`,
+      },
+    },
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
-    // {
-    //   resolve: "gatsby-source-filesystem",
-    //   options: {
-    //     name: "images",
-    //     path: `${__dirname}/src/images`,
-    //   },
-    // },
     {
       resolve: "gatsby-plugin-manifest",
       options: {
