1bb3451a9db174a452a20130ce78dfbba1379a3b
Andrey Okonetchnikov <andrey@okonet.ru>
2020-07-06 16:21:23 +0200

    Improve About page

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -1,12 +1,40 @@
 /* @jsx jsx */
 import React from "react"
-import { jsx } from "theme-ui"
+import { jsx, Box } from "theme-ui"
 import Hint from "../components/Hint"
 import Ingredients from "../components/Ingredients"
 import Preparation from "../components/Preparation"
 
 export default {
   blockquote: Hint,
+  dl: (props) => (
+    <Box
+      as="dl"
+      sx={{
+        display: "grid",
+        gridTemplateColumns: "auto 1fr",
+        gridGap: 3,
+        alignItems: "baseline",
+        my: 3,
+        dt: {
+          m: 0,
+          variant: "textStyles.sectionTitle",
+          p: {
+            color: "muted",
+            my: 0,
+          },
+        },
+        dd: {
+          m: 0,
+          variant: "textStyles.body",
+          p: {
+            my: 0,
+          },
+        },
+      }}
+      {...props}
+    />
+  ),
   Ingredients,
   Preparation,
 }
