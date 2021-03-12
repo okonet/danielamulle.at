618447da128a4cbfb040f7f90b178eeff7f218e0
Andrey Okonetchnikov <andrey@okonet.ru>
2021-03-12 11:59:15 +0100

    Migrate CMS

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,39 +1,19 @@
-import Head from "next/head"
 import dynamic from "next/dynamic"
 
-// import config from "../cms/config"
-// import CMS from "netlify-cms-app"
-// import { Control, Preview } from "@ncwidgets/file-relation"
 const CMS = dynamic(
   () =>
     import("netlify-cms-app").then((cms) => {
-      cms.init()
+      import("@ncwidgets/file-relation").then((widget) => {
+        console.log(widget)
+        const { Control, Preview } = widget
+        cms.registerWidget("file-relation", Control, Preview)
+        cms.init()
+      })
     }),
-  { ssr: false, loading: () => <p>Loading...</p> }
+  { ssr: false, loading: () => <p>Loading CMS...</p> }
 )
 
-// CMS.registerWidget("file-relation", Control, Preview)
-
 function AdminPage() {
-  return (
-    <>
-      <CMS />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            if (window.netlifyIdentity) {
-            window.netlifyIdentity.on("init", user => {
-            console.log(user)
-                if (!user) {
-                    window.netlifyIdentity.on("login", () => {
-                        document.location.href = "/admin/";
-                    });
-                }
-            });
-        `,
-        }}
-      />
-    </>
-  )
+  return <CMS />
 }
 export default AdminPage
