dfcbd107860e4958c7cbbf6ec65cbaba01a4d06a
Andrey Okonetchnikov <andrey@okonet.ru>
2021-05-03 10:03:47 +0200

    Set cache control for images

--- next.config.js
+++ next.config.js
@@ -3,4 +3,18 @@ module.exports = {
     locales: ["de-AT"],
     defaultLocale: "de-AT",
   },
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|webp|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, must-revalidate",
+          },
+        ],
+      },
+    ]
+  },
 }
