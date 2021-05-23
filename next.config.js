1e82c9800c14e2eebbe0a5f6eb8d92a15cb5bb4b
Andrey Okonetchnikov <andrey@okonet.ru>
2021-05-23 15:14:34 +0200

    Add redirects for existing links /impressum and /datenschutz

--- next.config.js
+++ next.config.js
@@ -3,4 +3,19 @@ module.exports = {
     locales: ["de-AT"],
     defaultLocale: "de-AT",
   },
+
+  async redirects() {
+    return [
+      {
+        source: "/impressum",
+        destination: "/legal/impressum",
+        permanent: true,
+      },
+      {
+        source: "/datenschutz",
+        destination: "/legal/datenschutz",
+        permanent: true,
+      },
+    ]
+  },
 }
